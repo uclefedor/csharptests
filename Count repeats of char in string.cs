e740f536d5b886c981a96d8d23a2544650b84c92
uclefedor <51266183+uclefedor@users.noreply.github.com>
2020-05-06 15:58:24 +0300

    All Star Code Challenge #18
    Create a function called that accepts 2 string arguments and returns an integer of the count of occurrences the 2nd argument is found in the first one.
    
    If no occurrences can be found, a count of 0 should be returned.
    
    strCount('Hello', 'o') // => 1
    strCount('Hello', 'l') // => 2
    strCount('', 'z')      // => 0
    Notes:
    
    The first argument can be an empty string
    The second string argument will always be of length 1

---
+++ Count repeats of char in string.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Kata
+{
+    public static int StrCount(string str, string letter)
+    {
+        int count = 0;
+        foreach (var s1 in str)
+        {
+          if (s1 == letter[0])
+          {
+            count++;
+          }
+        }
+        return count;
+    }
+}

58acae75e5c1ae9988c7730e6398c33b746a738b
uclefedor <51266183+uclefedor@users.noreply.github.com>
2020-05-06 15:59:15 +0300

    sample test

---
+++ test Count repeats of char in string.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+[TestFixture]
+class Tets
+{
+    [TestCase("Hello", "o", 1)]
+    [TestCase("Hello", "l", 2)]
+    [TestCase("", "z", 0)]
+    public void BasicTetst(string a, string b, int expected)
+    {
+        Assert.That(Kata.StrCount(a, b), Is.EqualTo(expected));
+    }
+}

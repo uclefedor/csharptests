dc6ec35fc726504124db75506b062939581a3792
uclefedor <51266183+uclefedor@users.noreply.github.com>
2020-05-06 14:05:20 +0300

    Sample Tests

---
+++ test Split In Parts.cs
@@ -0,0 +1,42 @@
+using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+[TestFixture]
+public class SplitIt
+{   
+
+    [TestCase]
+    //Fixed values
+    public void splitString1()  
+    {
+       string str = "supercalifragilisticexpialidociouss";       
+       Assert.AreEqual("sup erc ali fra gil ist ice xpi ali doc iou ss", Kata.SplitInParts(str,3));
+    }
+    
+    [TestCase]
+    //Fixed values
+    public void splitString2()  
+    {
+       string str = "HelloKata";     
+       Assert.AreEqual("Hel loK ata" , Kata.SplitInParts(str, 3));
+    }  
+    
+    [TestCase]
+    //Fixed values
+    public void splitString3()  
+    {
+       string str = "HelloKata";     
+       Assert.AreEqual("H e l l o K a t a" , Kata.SplitInParts(str, 1));
+    } 
+    
+    [TestCase]
+    //Fixed values
+    public void splitString4()  
+    {
+       string str = "HelloKata";     
+       Assert.AreEqual("HelloKata" , Kata.SplitInParts(str, 9));
+    } 
+}
+    

2bc7262351b2e18199a8c960c3380e2345b7016b
uclefedor <51266183+uclefedor@users.noreply.github.com>
2020-05-06 14:08:13 +0300

    Return the first M multiples of N
    Implement a function, multiples(m, n), which returns an array of the first m multiples of the real number n. Assume that m is a positive integer.
    
    Ex.
    
    multiples(3, 5.0)
    should return
    
    [5.0, 10.0, 15.0]

---
+++ Return the first M multiples of N.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Kata
+{
+  public static double[] Multiples(int m, double n)
+  {
+    double[] result = new double[m];
+    for (int i = 0; i < m; i++)
+      {
+         result[i] = (i+1)*n;
+      }
+    return result;
+  }
+}

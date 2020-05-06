0f35dcca1975a8e80fc7bc23e42cf521445cdb21
uclefedor <51266183+uclefedor@users.noreply.github.com>
2020-05-06 14:02:54 +0300

    Split In Parts
    The aim of this kata is to split a given string into different strings of equal size (note size of strings is passed to the method)
    
    Example:
    
    Split the below string into other strings of size #3
    
    'supercalifragilisticexpialidocious'
    
    Will return a new string
    'sup erc ali fra gil ist ice xpi ali doc iou s'
    Assumptions:
    
    String length is always greater than 0
    String has no spaces
    Size is always positive

---
+++ Split In Parts.cs
@@ -0,0 +1,52 @@
+using System;
+
+public class Kata
+{
+  public static string SplitInParts(string s, int partLength)
+  {  
+ //declare variables
+ 
+    string newString = "";
+    string temp = "";
+    int size = 0;
+    string tail = "";
+  
+ //size of array + tail 
+   
+    if (s.Length<=partLength)
+      {
+        newString = s;
+      }
+    if (s.Length%partLength != 0)
+      {
+        size = s.Length/partLength + 1;
+        int indexOfTail = s.Length - s.Length%partLength;
+        tail = s.Substring(indexOfTail);
+      }
+    else
+      {
+       size = s.Length/partLength; 
+       Console.WriteLine(size);
+      }
+    if (s.Length<=partLength)
+      {
+        newString = s;
+        Console.WriteLine(size);
+      }
+   
+  //create new result string
+    
+    string[] sArr = new string[size];
+    int count = 0;
+    {
+      for (int i = 0; i<=s.Length-partLength; i+=partLength)
+      {
+        temp = s.Substring(i, partLength);
+        sArr[count] = temp;
+        count++;
+      }
+    }
+    newString = String.Join(" ", sArr);
+    return newString + tail;    
+  }
+}  

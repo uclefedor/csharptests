7855732cbbc5ecc520d45e61e7c9ff7b1953e81d
uclefedor <51266183+uclefedor@users.noreply.github.com>
2020-05-13 23:13:43 +0300

    test Naughty or Nice
    sample tests

---
+++ test Naughty or Nice.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+[TestFixture]
+public class NaughtyOrNiceTest
+{
+  [Test]
+  public void NaughtyTest()
+  {
+    var naughty = new List<Person> {
+      new Person { Name = "Marco", WasNice =  false},
+      new Person { Name = "Luc", WasNice =  false},
+    };
+    var nice = new List<Person> {
+      new Person { Name = "Ryan", WasNice =  true},
+      new Person { Name = "Barak", WasNice =  true},
+    };
+
+    var both = naughty.Concat(nice);
+
+    Assert.AreEqual(0, NaughtyOrNice.GetNiceNames(naughty).Count(), "Wrong number of nice names");
+    Assert.AreEqual(0, NaughtyOrNice.GetNaughtyNames(nice).Count(), "Wrong number of naughty names");
+    Assert.AreEqual("Ryan,Barak", String.Join(",", NaughtyOrNice.GetNiceNames(nice)));
+    Assert.AreEqual("Marco,Luc", String.Join(",", NaughtyOrNice.GetNaughtyNames(naughty)));
+    Assert.AreEqual("Ryan,Barak", String.Join(",", NaughtyOrNice.GetNiceNames(both)));
+    Assert.AreEqual("Marco,Luc", String.Join(",", NaughtyOrNice.GetNaughtyNames(both)));
+  }
+}

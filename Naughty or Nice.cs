68213b7f11a88f2e5fcc6e29638696c280cfae10
uclefedor <51266183+uclefedor@users.noreply.github.com>
2020-05-13 23:11:59 +0300

    Create Naughty or Nice
    Happy Holidays fellow Code Warriors!
    It's almost Christmas! That means Santa's making his list, and checking it twice. Unfortunately, elves accidentally mixed the Naughty and Nice list together! Santa needs your help to save Christmas!
    
    Save Christmas!
    Santa needs you to write two functions. Both of the functions accept a sequence of objects. The first one returns a sequence containing only the names of the nice people, and the other returns a sequence containing only the names of the naughty people. Return an empty sequence [] if the result from either of the functions contains no names.
    
    The objects in the passed will represent people. Each object contains two properties: name and wasNice.
    name - The name of the person
    wasNice - True if the person was nice this year, false if they were naughty
    
    Person Object Examples
    Class Person is pre-loaded with the properties:
    
    public string Name { get; set; }
    public bool WasNice { get; set; }

---
+++ Naughty or Nice.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+
+public class Person
+{
+  public string Name { get; set; }
+  public bool WasNice { get; set; }
+}
+
+public class NaughtyOrNice
+{
+  /*search for nice people: 
+  new List<Person> {
+  new PersonList{ Name = "Marco", WasNice =  false} Person.wasNaughty = "Marco" => Person.WasNice == true;*/
+  public static IEnumerable<string> GetNiceNames(IEnumerable<Person> people)
+  {
+    List<string> result = new List<string>();
+    foreach (var man in people)
+    if (man.WasNice == true)
+    {
+      result.Add(man.Name);  
+    }
+    return result;
+  }
+  
+  /*search for naughty people: 
+  new List<Person> {
+  new PersonList{ Name = "Marco", WasNice =  false} Person.wasNaughty = "Marco" => Person.WasNice == false;*/
+  public static IEnumerable<string> GetNaughtyNames(IEnumerable<Person> people)
+  {
+    List<string> result = new List<string>();
+    foreach (var man in people)
+    if (man.WasNice == false)
+    {
+      result.Add(man.Name); 
+    }
+    return result;
+  }
+}

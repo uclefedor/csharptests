b6add0b78939faa12bc2cafe3c9a910f67352329
uclefedor <51266183+uclefedor@users.noreply.github.com>
2020-05-13 23:06:18 +0300

    test Unique string characters
    sample tests

---
+++ test Unique string characters.cs
@@ -0,0 +1,18 @@
+namespace Solution 
+{
+  using NUnit.Framework;
+  using System;
+
+  [TestFixture]
+  public class SolutionTest
+  {
+    [Test]
+    public void SampleTest()
+    {
+      Assert.That(Kata.Solve("xyab", "xzca"), Is.EqualTo("ybzc"));
+      Assert.That(Kata.Solve("xyabb", "xzca"), Is.EqualTo("ybbzc"));
+      Assert.That(Kata.Solve("abcd", "xyz"), Is.EqualTo("abcdxyz"));
+      Assert.That(Kata.Solve("xxx", "xzca"), Is.EqualTo("zca"));
+    }
+  }
+}
